a445c0aa75bc39e6c66fb99b5b37a415bf8bcdb8
JDownloader <jeandanielpearson@gmail.com>
2014-12-18 15:29:42 -0500

    main

---
+++ sample/__main__.py
@@ -0,0 +1,8 @@
+from Hello import Hello
+
+def main():
+    hey = Hello()
+    hey.say_hello()
+
+if __name__ == "__main__":
+    main()
---
+++ sample/hello.py
@@ -0,0 +1,14 @@
+import os
+
+"""
+Open additional data files using the absolute path,
+otherwise it doesn't always find the file.
+"""
+# The absolute path of the directoy for this file:
+_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+class Hello:
+    def __init__(self):
+        return None
+    def say_hello(self):
+        print "Hello, World!"
