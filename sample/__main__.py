123d13a4c1a49f90adfd169f89afe15c165a4713
teanutella <tea.rudolf@gmail.com>
2014-12-20 20:47:59 -0500

    adding project format to gitignore

--- sample/__main__.py
+++ sample/__main__.py
@@ -1,8 +1,10 @@
-from Hello import Hello
+from hello import Hello
+
 
 def main():
     hey = Hello()
     hey.say_hello()
+
 
 if __name__ == "__main__":
     main()
--- sample/hello.py
+++ sample/hello.py
@@ -7,8 +7,10 @@ otherwise it doesn't always find the file.
 # The absolute path of the directoy for this file:
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 
+
 class Hello:
     def __init__(self):
         return None
+
     def say_hello(self):
         print "Hello, World!"
