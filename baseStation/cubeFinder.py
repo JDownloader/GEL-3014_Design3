604fde0b75d6e3b4903a599d7232e18455b16e76
JDownloader <jeandanielpearson@gmail.com>
2015-03-11 15:08:02 -0400

    base cubefinder

---
+++ baseStation/cubeFinder.py
@@ -0,0 +1,17 @@
+from
+
+
+
+
+class CubeFinder():
+    def __init__(self):
+        pass
+    def get_all_cubes(self):
+        return None
+    def add_cube(self, cube):
+        pass
+
+
+class DemoCubeFinder(CubeFinder):
+    def __init__(self):
+        CubeFinder.__init__(self)
