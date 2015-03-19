01e3c8d3034bd37f3f9cb9008a3857b5ce27c410
andrearuiz <andrearuizg@gmail.com>
2015-03-19 13:22:53 -0400

    detection cubes sur la camera

--- vision/camera.py
+++ vision/camera.py
@@ -1,9 +1,11 @@
 import os
 import cv2
 import numpy as np
+import math
 
 CAMERA_MATRIX = [[555.81341097, 0. ,323.81267756], [0., 555.07406939, 244.96419131], [0., 0.,1.]]
 DIST_COEFS = [[0.26812386, -1.12309446,  0.00970802, -0.00178942,  0.97162928]]
+ANGLE = 45
 
 class Camera():
 
@@ -32,7 +34,7 @@ class Camera():
             if area > max_area:
                 max_area = area
                 largest_contour = contour
-        if not largest_contour == None:
+        if largest_contour is not None:
             moment = cv2.moments(largest_contour)
             if moment["m00"] > 1000: # / scale_down:
                 marker = cv2.minAreaRect(largest_contour)
@@ -41,3 +43,23 @@ class Camera():
 
     def get_angle_cube(self, marker):
         return marker[2]
+
+    def find_distance(self, marker, height_img, width_img):
+        box = cv2.cv.BoxPoints(marker)
+        box = np.int0(box)
+        topedge = pow((box[3][0] - box[2][0]) , 2) + pow((box[3][1] - box[2][1]) , 2)
+        upedge = pow((box[0][0] - box[1][0]) , 2) + pow((box[0][1] - box[1][1]) , 2)
+        widthTop = math.sqrt(topedge)
+        widthBottom = math.sqrt(upedge)
+        leftedge = pow((box[1][0] - box[2][0]) , 2) + pow((box[1][1] - box[2][1]) , 2)
+        rigthedge = pow((box[0][0] - box[3][0]) , 2) + pow((box[0][1] - box[3][1]) , 2)
+        heightLeft = math.sqrt(leftedge)
+        heightRight = math.sqrt(rigthedge)
+        dArea = height_img*width_img - ((heightLeft+heightRight)/2)*((widthTop+widthBottom)/2)
+        dz = 30 + dArea/100
+        dy = ((240 - ((box[2][1] + box[3][1])/2 + (heightLeft + heightRight)/4 ))*dz)/585
+        angleB = math.atan(dy/dz)
+        distance = ((dz/math.cos(-angleB))*math.cos(-ANGLE*math.pi/180+angleB))/100
+
+        return distance
+
