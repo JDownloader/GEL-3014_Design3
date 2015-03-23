9670e49f6200d5cc4639d788f167f2d279fd20ad
Jean-Daniel Pearson <jeandanielpearson@gmail.com>
2015-03-23 15:00:03 -0400

    Kinect is mocked message in UI

--- baseStation/runLoop.py
+++ baseStation/runLoop.py
@@ -1,6 +1,7 @@
 import time
 import random
-from vision.kinect import Kinect
+from vision.kinect import Kinect, NoKinectDetectedException
+from tests.test_vision import FakeKinect
 from cubeFinder import CubeFinder, DemoCubeFinder
 from time import gmtime, strftime
 from flagLoop import FlagLoop
@@ -10,7 +11,10 @@ class RunLoop:
 
     def __init__(self):
         self.flag_loop = FlagLoop()
-        self.kinect = Kinect()
+        try:
+            self.kinect = Kinect()
+        except NoKinectDetectedException:
+            self.kinect = FakeKinect()
         self.cube_finder = DemoCubeFinder(self.kinect)
 
     def start(self):
@@ -27,12 +31,13 @@ class RunLoop:
     def get_status(self, robot_ip):
         pos_y = random.randrange(0, 400, 1)
         run_time = self.get_time()
-        sample_status = { "top": 30,
-                          "left": pos_y,
-                          "chrono": strftime("%Mm%Ss",gmtime(run_time)),
-                          "robotIP": robot_ip,
-                          "flag": self._get_current_flag(),
-                          "cubes": self.get_cubes()}
+        sample_status = { 'top': 30,
+                          'left': pos_y,
+                          'kinect_is_fake': self.is_fake_kinect(),
+                          'chrono': strftime('%Mm%Ss', gmtime(run_time)),
+                          'robotIP': robot_ip,
+                          'flag': self._get_current_flag(),
+                          'cubes': self.get_cubes()}
         # "cubes": [[20, 20 , 'red'], [20, 100, 'blue']]
         return sample_status
 
@@ -42,3 +47,8 @@ class RunLoop:
         for cube in self.cube_finder.cubes:
             cubes_positions.append([cube.position[0], cube.position[1], cube.color])
         return cubes_positions
+
+    def is_fake_kinect(self):
+        if isinstance(self.kinect, FakeKinect):
+            return True
+        return False
