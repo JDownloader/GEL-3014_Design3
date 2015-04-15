1f1718b31d3b09a7e73d0b439c350e85811a3dea
Jean-Daniel Pearson <jeandanielpearson@gmail.com>
2015-04-15 03:08:10 -0400

    stuufdfrwesg

--- robot/baseStationClient.py
+++ robot/baseStationClient.py
@@ -24,6 +24,5 @@ class BaseStationClient():
 
     def send_pathfinding_itinerary(self, path):
         print path
-        raw_data = {'path': path}
-        data = json.dumps(raw_data)
-        requests.post('http://' + self.app.base_station_ip_address + ':8000' + cte.PATH_RESSOURCE, data=data)
+        raw_data = {'path': str(path)}
+        requests.post('http://' + self.app.base_station_ip_address + ':8000' + cte.PATH_RESSOURCE, data=raw_data)
