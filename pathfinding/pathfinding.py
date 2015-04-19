0ceb6b3975e1b67a2d632e26261ec9f9c5b2a7c7
Gabriel Grenon <grenon.gabriel@gmail.com>
2015-04-18 23:19:24 -0400

    code clean A1 merci.

--- pathfinding/pathfinding.py
+++ pathfinding/pathfinding.py
@@ -3,22 +3,9 @@ import constants
 
 
 class Pathfinding:
-
-    def find_path_to_cube_buffer_zone(self, robot_position, cube_center):
-        unbuffered_path = self.find_path_to_point(robot_position, cube_center)
-        distance_with_buffer = unbuffered_path[1] - constants.CUBE_BUFFER_RADIUS
-        return (unbuffered_path[0], distance_with_buffer)
-
-    def find_path_to_cube(self, cube_angle_compared_to_robot):
-        pass
-
     def find_path_to_point(self, robot_position, point):
         delta_x = float(point[0] - robot_position.position[0])
         delta_y = float(point[1] - robot_position.position[1])
-        print 'pathfinding to point'
-        print 'delta x ' + str(delta_x)
-        print 'delta_y ' + str(delta_y)
-        print 'angle ' + str(robot_position.angle)
         # We assume 0 degree is when the robot is facing away from the kinect, 90 degrees from the x axis.
         # For a positive angle, the robot must rotate to its left (anti-clockwise)
         target_angle = 0
@@ -29,7 +16,6 @@ class Pathfinding:
                 target_angle = -90
         else:
             target_angle = math.degrees(math.atan2(delta_x, delta_y))
-        print 'pathfinding target angle' + str(target_angle)
         rotation = self.determine_rotation_angle(robot_position.angle, target_angle)
         distance = self.calculate_hypotenuse(delta_x, delta_y)
         return (rotation, distance)
